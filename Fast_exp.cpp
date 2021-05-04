6719fabfdb0cadc02b62aa09cbb161f73276c448
Suman Kumar <48875251+18IIT30022@users.noreply.github.com>
2021-05-04 16:39:39 +0530

    Create Fast_exp.cpp

---
+++ Fast_exp.cpp
@@ -0,0 +1,17 @@
+lli mod=1e9+7;
+lli binary_exponentiation(lli a, lli b, lli mod) 
+{
+    lli ans =1;
+    while(b>0)
+    {
+        if(b%2==1)
+        {
+            ans = (ans*a) % mod;
+        }
+        a=(a*a) % mod;
+        b/=2;
+    }
+
+    return ans;
+}
+cout << binary_exponentiation(a,1e9+7-2,mod);

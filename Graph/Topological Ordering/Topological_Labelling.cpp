8db42b7beab46f7911f2ef04e911057db4969461
Suman Kumar <48875251+Suman16iitkgp@users.noreply.github.com>
2022-12-11 15:36:51 +0530

    Adding Topo Good Problem set

---
+++ Graph/Topological Ordering/Topological_Labelling.cpp
@@ -0,0 +1,177 @@
+/*
+Description
+You are given a directed acyclic graph with vertices and edges. There are no self-loops or multiple edges between anyYou should assign labels to all vertices in such a way that:
+Labels form a valid permutation of length — an integer sequence such that each integer from to appears exactlIf there exists an edge from vertex to vertex then should be smaller than .
+Permutation should be lexicographically smallest among all suitable.
+Find such a sequence of labels to satisfy all the conditions.
+*/
+
+/* Suman Kumar*/
+
+// #ifdef LOCAL
+// #include "bits/stdc++.h"
+// #else
+// #include <bits/stdc++.h>
+// #endif
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstring>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <valarray>
+#include <iterator>
+#include <functional>
+#include <limits>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cassert>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template<class T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+// template<class key, class value, class cmp = std::less<key>>
+// using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define ii pair<lli,lli>
+#define rep(i,a,b) for(lli i=a;i<=b;i++)
+#define rrep(i,a,b) for(lli i=a;i>=b;i--)
+#define sz(x) ((int)(x).size())
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x...)
+#endif
+
+typedef long long int lli;
+typedef unsigned long long ulli;
+typedef long double lld;
+
+void _print(lli t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ulli t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+lli gcd(lli a, lli b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+lli expo(lli a, lli b, lli mod) {lli res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+lli mminvprime(lli a, lli b) {return expo(a, b - 2, b);}
+bool revsort(lli a, lli b) {return a > b;}
+lli combination(lli n, lli r, lli m, lli *fact, lli *ifact) {lli val1 = fact[n]; lli val2 = ifact[n - r]; lli val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+vector<lli> sieve(int n) {int*arr = new int[n + 1](); vector<lli> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+lli mod_add(lli a, lli b, lli m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+lli mod_mul(lli a, lli b, lli m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+lli mod_sub(lli a, lli b, lli m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+lli mod_div(lli a, lli b, lli m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+lli n, m;
+vector<vector<lli> > g;
+vector<lli> outdeg;
+vector<lli> topo;
+
+// void dfs( lli nn ){
+//     vis[nn] = 1;
+//     for( auto v:g[nn] ){
+//         if( !vis[v] ){
+//             dfs(v);
+//         }
+//     }
+
+//     topo.push_back( nn );
+// }
+
+void solve(){
+    cin >> n >> m;
+    g.resize( n+1 );
+    outdeg.resize( n+1, 0 );
+    for( lli i=1; i<=m; i++ ){
+        lli a, b;
+        cin >> a >> b;
+        g[b].push_back( a );
+        outdeg[a]++;
+    }
+
+    vector<lli> col(n+1, 0);
+    priority_queue<lli> pq;
+    for( lli i=1; i<=n; i++ ){
+        if( outdeg[i] == 0ll ){
+            pq.push( i );
+        }
+    }
+    lli cc = n;
+    while( !pq.empty() ){
+        lli cr = pq.top();
+        pq.pop();
+        // cr *= -1;
+        // topo.push_back( cr );
+        col[cr] = cc;
+        cc--;
+        for( auto v:g[cr] ){
+            outdeg[v]--;
+            if( outdeg[v] == 0ll ){
+                pq.push( v );
+            }
+        }
+    }
+
+    for( lli i=1; i<=n; i++ ){
+        cout << col[i] << " ";
+    }
+    cout << endl;
+
+}
+
+signed main()
+{
+    fastio();
+    lli _t=1ll; 
+    // cin >> _t;
+    // clock_t begin=clock();
+    while(_t--)
+    {
+        solve();
+    }
+    // clock_t end = clock();double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    // cerr << endl;
+    // cerr << "Time: " << elapsed_secs  << endl;
+    return 0;
+}

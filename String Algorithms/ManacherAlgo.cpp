77ccf62668028e05bd16b59629d56ba1b084b1b6
Suman Kumar <48875251+Suman16iitkgp@users.noreply.github.com>
2024-11-15 17:42:58 +0100

    Update ManacherAlgo.cpp

--- String Algorithms/ManacherAlgo.cpp
+++ String Algorithms/ManacherAlgo.cpp
@@ -7,61 +7,46 @@ For example, the longest palindrome in "algoazzaa" is "azza". If there are multi
 Output the one which comes first in the string.
 */
 
-#define ll int64_t
-#define endl '\n'
+string longestPalindrome(string s) {
+    string sManacher = "&";
+    for( char ch:s ){
+        sManacher += ch;
+        sManacher += '&';
+    }
+    int n = int(sManacher.length());
+    vector<int> palindromeRadius(n, 0);
 
+    int center = 0, radius = 0;
 
-class Solution3 { // O(N) using manacher algorithm.
-public:
-    vector<int> manacher(string s) { // gives 2n-1 size manacher array. (a#c#...#d )
-        string s1 = "@"; for (auto c : s) s1 += c, s1 += "#";
-        s1.back() = '&';
-        vector<int> ans(s1.size() - 1); int lo = 0, hi = 0;
-        for (int i = 1; i <= s1.size() - 2; i++) {
-            if (i != 1) ans[i] = min(hi - i, ans[hi - i + lo]);
-            while (s1[i - ans[i] - 1] == s1[i + ans[i] + 1]) ans[i] ++;
-            if (i + ans[i] > hi) lo = i - ans[i], hi = i + ans[i];
+    for( int i=0; i<n; i++ ){
+        int mirror = 2*center - i;
+
+        if( i < radius ){
+            palindromeRadius[i] = min(radius-i, palindromeRadius[mirror]);
         }
-        ans.erase(begin(ans));
-        for (int i = 0; i <= ans.size() - 1; i++) if ((i & 1) == (ans[i] & 1)) ans[i] ++;
-        return ans;
-    }
-    string getLPS(string s) { // gives one of the long. palindromic substr.
-        auto a = manacher(s);
-        auto it = max_element(a.begin(), a.end());
-        int l = *it, p;
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] == l) {
-                p = i;
-                break;
-            }
+
+        while( i-palindromeRadius[i]-1 >= 0 && i+palindromeRadius[i]+1 < n && 
+        sManacher[i-palindromeRadius[i]-1] == sManacher[i+palindromeRadius[i]+1] ){
+            palindromeRadius[i]++;
         }
-        return s.substr((p + 1) / 2 - l / 2, l);
-    }
-};
 
-void solve() {
-    string s;
-    cin >> s;
-    Solution3 s3;
-    auto ans3 = s3.getLPS(s);
-    cout << ans3 << endl;
-}
-int main() {
-    ios_base :: sync_with_stdio(0);
-    cin.tie(nullptr); cout.tie(nullptr);
-
-#ifdef Mastermind_
-    freopen("input.txt", "r", stdin); \
-    freopen("output.txt", "w", stdout);
-#endif
-    int t = 1;
-    // int i = 1;
-    cin >> t;
-    while (t--) {
-        // cout << "Case #" << i << ": ";
-        solve();
-        // i++;
+        if( i+ palindromeRadius[i] > radius ){
+            center = i;
+            radius = i+ palindromeRadius[i];
+        }
     }
-    return 0;
+
+    int maxLength = 0;
+    int centerIndex = 0;
+    for( int i=0; i<palindromeRadius.size(); i++ ){
+        if( palindromeRadius[i] > maxLength ){
+            maxLength = palindromeRadius[i];
+            centerIndex = i;
+        }
+    }
+
+    int startIndex = (centerIndex-maxLength)/2;
+    string res = s.substr(startIndex, maxLength);
+
+    return res;
 }
